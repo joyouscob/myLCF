6343672135f553ea7a3f91c346121d707cf7d38f
James Cartwright <james@perspectivedesign.co.uk>
2013-04-25 22:06:17 +0100

    Modified CSS folder structure and created wordpress structure
    Created base .php files for wordpress theme and modified less files to
    compile into mylcf.css, sass files now compile into flat-ui.css

---
+++ footer.php
@@ -0,0 +1,82 @@
+<footer>
+	  		<div class="container">
+		  		<div class="row">
+		  			<div class="span7">
+		  				<h3 class="footer-title">This is the footer title</h3>
+		  				<p class="footer-p">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+		  				<a class="footer-brand" href="http://www.perspectivedesign.co.uk" target=_blank><img src="images/assets/logo-fade.png">Copyright 2013 Perspective Design</a>
+		  			</div>
+		  			<div class="span5">
+		  				<div class="footer-banner">
+		  					<h3 class="footer-title">Other Information</h3>
+		  					<ul>
+		  						<li><a href="#">News</a></li>
+		  						<li><a href="#">Our Building</a></li>
+		  						<li><a href="#">Planning Center</a></li>
+		  						<li><a href="#">Leadership</a></li>
+		  						<li><a href="#">Podcast</a></li>
+		  					</ul>
+		  					This is some other footer text with a <a href="#">Hyperlink</a>
+		  				</div>
+		  			</div>
+		  		</div>	
+	  		</div>
+	  	</footer>
+	
+
+    <!-- Le javascript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.8.2.min.js"></script>
+    <script src="js/bootstrap.js"></script>  
+       
+    <script>
+	function fixHeight() {
+		var currentTallest = 0,
+		     currentRowStart = 0,
+		     rowDivs = new Array(),
+		     $el,
+		     topPosition = 0;
+		 $('.height-fix').each(function() {
+		
+		   $el = $(this);
+		   topPostion = $el.position().top;
+		   
+		   if (currentRowStart != topPostion) {
+		
+		     // we just came to a new row.  Set all the heights on the completed row
+		     for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		       rowDivs[currentDiv].height(currentTallest);
+		     }
+		
+		     // set the variables for the new row
+		     rowDivs.length = 0; // empty the array
+		     currentRowStart = topPostion;
+		     currentTallest = $el.height();
+		     rowDivs.push($el);
+		
+		   } else {
+		
+		     // another div on the current row.  Add it to the list and check if it's taller
+		     rowDivs.push($el);
+		     currentTallest = (currentTallest < $el.height()) ? ($el.height()) : (currentTallest);
+		
+		  }
+		   
+		  // do the last row
+		   for (currentDiv = 0 ; currentDiv < rowDivs.length ; currentDiv++) {
+		     rowDivs[currentDiv].height(currentTallest);
+		   }
+		   
+		 });
+		}
+		
+	fixHeight();
+
+	$(window).resize(function(){
+    	fixHeight();
+    	})		
+    </script>
+     	  
+  </body>
+</html>
---
+++ header.php
@@ -0,0 +1,91 @@
+<?php
+/**
+ * The Header for our theme.
+ *
+ * Displays all of the <head> section and everything up to end of navigation>
+ *
+ * @package WordPress
+ * @subpackage smpl
+ * @since smpl 0.1
+ */
+?>
+
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <title>Louth Christian Fellowship</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <!-- Le styles -->
+    <link href="wp-content/themes/mylcf/css/mylcf.css" rel="stylesheet">
+    <style type="text/css">
+      body {
+        padding-top: 60px;
+        padding-bottom: 40px;
+      }
+    </style>
+        
+    <!-- Import additional syles from Flat UI -->
+    <link href="wp-content/themes/mylcf/css/flat-ui.css" rel="stylesheet">
+
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements -->
+    <!--[if lt IE 9]>
+      <script src="js/html5shiv.js"></script>
+    <![endif]-->
+
+    <!-- Fav and touch icons -->
+    <link rel="apple-touch-icon-precomposed" sizes="144x144" href="../assets/ico/apple-touch-icon-144-precomposed.png">
+    <link rel="apple-touch-icon-precomposed" sizes="114x114" href="../assets/ico/apple-touch-icon-114-precomposed.png">
+      <link rel="apple-touch-icon-precomposed" sizes="72x72" href="../assets/ico/apple-touch-icon-72-precomposed.png">
+                    <link rel="apple-touch-icon-precomposed" href="../assets/ico/apple-touch-icon-57-precomposed.png">
+                                   <link rel="shortcut icon" href="../assets/ico/favicon.png">
+  </head>
+
+  <body>
+	<div class="container palette-white">
+		<div class="row demo-row">
+			<div class="span6">
+				<div class="lcf-logo"><img src="images/assets/logo.png"></div>
+			</div>
+			<div class="span6">
+				<div class="social hidden-phone">
+					<ul>
+					<li><a href="#"><img src="images/assets/twitter.png"></a></li>
+					<li><a href="#"><img src="images/assets/map.png"></a></li>
+					<li><a href="#"><img src="images/assets/podcast.png"></a></li>
+					<li><a href="#"><img src="images/assets/instagram.png"></a></li>
+					<li><a href="#"><img src="images/assets/facebook.png"></a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div class="row">
+			<div class="span12">
+				<div class="navbar navbar-inverse">
+					<div class="navbar-inner">
+						<div class="container">
+							<!-- .btn-navbar is used as the toggle for collapsed navbar content -->
+						      <a class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						        <span class="icon-bar"></span>
+						      </a>
+						    <div class="nav-collapse collapse">
+								<ul class="nav">
+									<li class="active"><a href="#">Home</a></li>
+									<li><a href="#">Services</a></li>
+									<li><a href="#">Route 96</a></li>
+									<li><a href="#">Connect</a></li>
+									<li><a href="#">News</a></li>
+									<li><a href="#">Contact</a></li>
+									<li><a href="#">Beliefs</a></li>
+									<li><a href="#">Children</a></li>
+								</ul>
+						    </div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div> <!-- End navigation -->
