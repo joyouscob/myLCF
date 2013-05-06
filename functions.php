86bab10c0d35c3ebb49be192d814f9916ac7d79b
James Cartwright <james@perspectivedesign.co.uk>
2013-05-06 22:24:40 +0100

    Widgetized footer area
    registered a widget and added formatting to match style of footer
    paragraph.

--- functions.php
+++ functions.php
@@ -4,4 +4,15 @@ register_nav_menus( array(
 	'primary_navigation' => 'Primary Navigation Menu',
 	'footer_menu' => 'Footer Navigation Menu'
 ) );
+
+// Register sidebar widget areas
+register_sidebar(array(
+  'name' => __( 'Footer Widget Area' ),
+  'id' => 'footer-widget',
+  'description' => __( 'Widgets in this area will appear in the left of the footer area.' ),
+  'before-widget' => '',
+  'after-widget' => '',
+  'before_title' => '<h3 class="footer-title">',
+  'after_title' => '</h3>'
+));
 ?>
