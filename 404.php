79e8040e449f3e3c4f644b4aa48281a85f0d3ed5
James Cartwright <james@perspectivedesign.co.uk>
2013-05-07 22:30:28 +0100

    Added 404 error page
    Added a custom 404 error page - untested

---
+++ 404.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage mylcf
+ * @since mylcf 0.1
+ */
+
+get_header();
+?>
+
+<div class="row">
+	<div class="span12">
+		<div style="text-align: center;">
+			<h1 style="position: relative; left: 20px;">404 Error: Page not found</h1>
+			<h3 style="position: relative; right: 40px;">It looks like the page you wanted has gone...</h3>
+			<h2 style="position: relative; left: 10px;">Maybe you could goto the homepage and try again</h2>
+		</div>
+	</div>
+</div>
+
+<?php get_footer(); ?>
+
+</body>
+</html>
